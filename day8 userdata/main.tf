28adc27215f32ba9b3b756bcfa3bdd80d4309200
richards28 <richirocks96@gmail.com>
2025-01-24 11:39:37 +0530

    Update main.tf

--- day8 userdata/main.tf
+++ day8 userdata/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "dev" {
     ami = "ami-0fb04413c9de69305"
     instance_type = "t2.micro"
-    key_name = "keypair2"
+    key_name = "keypair1"
     user_data = file("script.sh")
     tags = {
       Name = "ec2"
